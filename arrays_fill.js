99acfdc74df35ca7fe3e4023e34c616b96fbec0a
Bruno Carvalho <32099971+BrunoCarvalho18@users.noreply.github.com>
2023-03-02 23:14:55 +0000

    commit
    commit

---
+++ arrays_fill.js
@@ -0,0 +1,5 @@
+let numbersCopy = Array.of(1,2,3,4,5,6);
+
+numbersCopy.fill(2,1);
+
+console.log(numbersCopy)// Preencherá todos 0
---
+++ remove_elements.js
@@ -0,0 +1,10 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+numbers.pop(); // remove the last one
+
+console.log(numbers)
+
+numbers.unshift(-2);
+numbers.unshift(-4,-3);
+
+console.log(numbers)
